a7b6166ac5e2fa257bd1d6737496526f50b5d0bd
Will Song <16807975+WillySong@users.noreply.github.com>
2024-03-10 14:40:58 -0400

    Adding custom pricing

--- components/pricing-tables.tsx
+++ components/pricing-tables.tsx
@@ -41,7 +41,7 @@ export default function PricingTables() {
               </div>
             </div> */}
 
-            <div className="max-w-sm md:max-w-2xl xl:max-w-none mx-auto grid gap-8 md:grid-cols-2 xl:grid-cols-3 xl:gap-6 items-start">
+            <div className="max-w-sm md:max-w-2xl xl:max-w-none mx-auto grid gap-8 md:grid-cols-2 xl:grid-cols-4 xl:gap-6 items-start">
 
               {/* Pricing table 1 */}
               <div className="relative flex flex-col h-full py-5 px-6 rounded bg-white shadow-xl" data-aos="zoom-y-out" data-aos-delay="450">
@@ -87,19 +87,19 @@ export default function PricingTables() {
                   </li>
                 </ul>
                 <div className="border-t border-gray-200 pt-5 mt-6">
-                  <a className="btn-sm text-white bg-blue-600 hover:bg-blue-700 w-full" href="#0">Request a 1-month free trial</a>
+                  <a className="btn-sm text-white bg-aip-purple-finn hover:bg-aip-purple-royal w-full" href="#0">Request a free trial</a>
                 </div>
               </div>
 
               {/* Pricing table 2 */}
-              <div className="relative flex flex-col h-full py-5 px-6 rounded bg-blue-100 shadow-xl border-2 border-blue-500" data-aos="zoom-y-out" data-aos-delay="450">
-                <div className="absolute top-0 right-0 mr-5 p-3 -mt-5 bg-yellow-500 rounded-full">
+              <div className="relative flex flex-col h-full py-5 px-6 rounded bg-aip-purple-pale shadow-xl border-2 border-aip-purple-dark" data-aos="zoom-y-out" data-aos-delay="450">
+                <div className="absolute top-0 right-0 mr-5 p-3 -mt-5 bg-aip-orange-sandy rounded-full">
                   <svg className="w-4 h-4 fill-current text-white" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                     <path d="M15.145 5.05l-4.316-.627L8.898.513c-.338-.684-1.456-.684-1.794 0l-1.93 3.91-4.317.627a1.002 1.002 0 00-.554 1.707l3.124 3.044-.737 4.3a1 1 0 001.45 1.053L8 13.125l3.862 2.03c.728.381 1.59-.234 1.45-1.054l-.736-4.299L15.7 6.758a1.003 1.003 0 00-.555-1.708z" />
                   </svg>
                 </div>
                 <div className="mb-4">
-                  <div className="text-lg font-bold mb-1">Small & Medium Business</div>
+                  <div className="text-lg font-bold mb-1">Business</div>
                   <div className="inline-flex items-baseline mb-2">
                     <span className="text-3xl font-bold">$</span>
                     <span className="text-4xl font-bold">{annual ? '600' : '800'}</span>
@@ -112,7 +112,7 @@ export default function PricingTables() {
                     <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                     </svg>
-                    <span>Unlimited pre-made AI Personas + 1 custom AI Personas</span>
+                    <span>Unlimited pre-made AI Personas + 1 customizable AI Persona</span>
                   </li>
                   <li className="flex items-center mb-2">
                     <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
@@ -140,7 +140,7 @@ export default function PricingTables() {
                   </li>
                 </ul>
                 <div className="border-t border-gray-200 pt-5 mt-6">
-                  <a className="btn-sm text-white bg-blue-600 hover:bg-blue-700 w-full" href="#0">Request a 1-month free trial</a>
+                  <a className="btn-sm text-white bg-aip-purple-finn hover:bg-aip-purple-royal w-full" href="#0">Request a free trial</a>
                 </div>
               </div>
 
@@ -160,7 +160,7 @@ export default function PricingTables() {
                     <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                     </svg>
-                    <span>Unlimited, unrestricted, self-improving, AI Personas</span>
+                    <span>Unlimited, unrestricted, self-improving, customizable AI Personas</span>
                   </li>
                   <li className="flex items-center mb-2">
                     <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
@@ -188,7 +188,53 @@ export default function PricingTables() {
                   </li>
                 </ul>
                 <div className="border-t border-gray-200 pt-5 mt-6">
-                  <a className="btn-sm text-white bg-blue-600 hover:bg-blue-700 w-full" href="#0">Request a 1-month free trial</a>
+                  <a className="btn-sm text-white bg-aip-purple-finn hover:bg-aip-purple-royal w-full" href="#0">Request a free trial</a>
+                </div>
+              </div>
+
+              {/* Pricing table 4 */}
+              <div className="relative flex flex-col h-full py-5 px-6 rounded bg-white shadow-xl" data-aos="zoom-y-out" data-aos-delay="450">
+                <div className="mb-4">
+                  <div className="text-lg font-bold mb-1">Custom</div>
+                  <div className="inline-flex items-baseline mb-2">
+                    <span className="text-4xl font-bold">{'Custom'}</span>
+                  </div>
+                  <div className="text-lg text-gray-800">A custom solution for your business needs.</div>
+                </div>
+                <ul className="text-gray-600 -mb-2 grow">
+                  <li className="flex items-center mb-2">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Hand-crafted AI Personas</span>
+                  </li>
+                  <li className="flex items-center mb-2">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Unlimited uploads per week</span>
+                  </li>
+                  <li className="flex items-center mb-2">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Unlimited accounts across your organization</span>
+                  </li>
+                  <li className="flex items-center mb-2">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Unlimited campaign saving</span>
+                  </li>
+                  <li className="flex items-center mb-2">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>24/7 support</span>
+                  </li>
+                </ul>
+                <div className="border-t border-gray-200 pt-5 mt-6">
+                  <a className="btn-sm text-white bg-aip-purple-finn hover:bg-aip-purple-royal w-full" href="#0">Request a free trial</a>
                 </div>
               </div>
 
