75b282e03d605bca435027ad0c4fd4bb2a40afc4
Will Song <16807975+WillySong@users.noreply.github.com>
2024-10-17 14:14:34 -0400

    Fixing tag and removing build warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-import './css/style.css'
+import Header from '@/components/ui/header'
 
 import { Inter } from 'next/font/google'
-
-import Header from '@/components/ui/header'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -22,28 +20,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script type="text/javascript">
+          {`
+            _linkedin_partner_id = "6703628";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `}
+        </script>
+        <script type="text/javascript">
+          {`
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+          `}
+        </script>
+      </head>
       <body className={`${inter.variable} font-inter antialiased bg-white text-gray-900 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
           <Header />
           {children}
         </div>
-        <script type="text/javascript">
-          _linkedin_partner_id = "6703628";
-          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-          </script><script type="text/javascript">
-          (function(l) {
-          if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
-          window.lintrk.q=[]}
-          var s = document.getElementsByTagName("script")[0];
-          var b = document.createElement("script");
-          b.type = "text/javascript";b.async = true;
-          b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-          s.parentNode.insertBefore(b, s);})(window.lintrk);
-          </script>
-          <noscript>
-          <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=6703628&fmt=gif" />
-          </noscript>
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}} alt="" src="https://px.ads.linkedin.com/collect/?pid=6703628&fmt=gif" />
+        </noscript>
       </body>
     </html>
   )
